e65be4659a2ba0d8fdfcfd6c4aa2e49921d5a161
Alex <alexengineering1027@gmail.com>
2018-10-19 23:41:43 -0600

    candy spawn

--- Assets/_scripts/CandyMechanics.cs
+++ Assets/_scripts/CandyMechanics.cs
@@ -5,34 +5,49 @@ public class CandyMechanics : MonoBehaviour
 {
     playerStats playerStatScript;
     public GameObject playerObject;
+    public GameObject[] spriteCandyList;
+
     //public Clock clock;
-    public GameObject candyToSpawn;
+
+    GameObject candyToSpawn;
     public float spawnTimer = 5f;
+
+    bool candySpawned;
     
     // Use this for initialization
     void Start()
     {
         playerObject = this.GetComponent<GameObject>();
         playerStatScript = this.GetComponent<playerStats>();
+
+        candySpawned = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         spawnTimer -= Time.deltaTime;
-        if (spawnTimer <= 0)
-            Spawn();
+        if (spawnTimer <= 0 && candySpawned == false)
+        {
+            candySpawned = true;
+            //candyToSpawn = spriteCandyList[Random.Range(0, 5)];
+            Instantiate(spriteCandyList[Random.Range(0, 5)]);
+        }
     }
+
     void Spawn()
     {
         Instantiate(candyToSpawn);
     }
-    void OnTriggerEnter(Collider other)
+
+    void OnTrigger(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             //implement a candy count to the player
-
+            Debug.Log("Collect candy");
+            candySpawned = false;
+            spawnTimer = 0;
             playerStatScript.candyAmount++;
             this.gameObject.SetActive(false);
             Destroy(this.gameObject);

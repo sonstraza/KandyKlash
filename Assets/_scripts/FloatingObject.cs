54f5b9fe74ec010b4cbc15ef3ec012edb40c44cd
singh1rocks <shakti.das@aol.com>
2018-10-19 23:41:40 -0600

    Floating Object

---
+++ Assets/_scripts/FloatingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour {
+
+    public GameObject floated;
+    private Rigidbody rb;
+    public float RotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, RotationSpeed, 0);
+		
+	}
+}

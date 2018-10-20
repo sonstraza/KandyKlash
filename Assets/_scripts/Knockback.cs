d53d87080839010f109591ceef6d9386b9c6bcb7
Schir <Schir@users.noreply.github.com>
2018-10-19 22:25:26 -0600

    added a basic knockback class

---
+++ Assets/_scripts/Knockback.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour {
+    public GameObject playerObject;
+    public Vector3 playerPosition;
+
+	// Use this for initialization
+	void Start () {
+        playerPosition = playerObject.transform.position;
+	}
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void KnockbackMethod()
+    {
+        playerPosition.x -= 5;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "MyGameObjectName")
+        {
+            KnockbackMethod();
+        }
+    }
+}

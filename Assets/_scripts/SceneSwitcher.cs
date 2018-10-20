8d87aac40227db128214a32bdf4ab481d1622118
PluggedInGamer <kaz861@gmail.com>
2018-10-19 21:39:35 -0600

    Menu Buttons

---
+++ Assets/_scripts/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void GotoPlayerSelectcene()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}

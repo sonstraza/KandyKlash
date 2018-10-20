6707fc376149d17544ddcbd56f00eeeeb8338987
Brigham777 <ighambray@outlook.com>
2018-10-19 20:42:11 -0600

    More sounds

---
+++ Assets/_scripts/bagAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bagAudio : MonoBehaviour {
+
+   
+    public AudioSource medHitSource;
+    public AudioSource bigHitSource;
+
+	// Use this for initialization
+	void Start () {
+       // medHitSource.clip medHit; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            medHitSource.Play();
+
+        if (Input.GetMouseButtonDown(0))
+            bigHitSource.Play();
+	}
+}

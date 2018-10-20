fca3501aa45f0960860d7f1ad373a223a53978f2
PluggedInGamer <kaz861@gmail.com>
2018-10-19 20:45:31 -0600

    Menu Scene & Working Clock

--- Assets/_scripts/Clock.cs
+++ Assets/_scripts/Clock.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Clock : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Clock : MonoBehaviour
+{
+    public int timeLeft = 30;
+    public Text countdownText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = ("Time Left = " + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times Up!";
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
 }
